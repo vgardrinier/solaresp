a2c89f908e1c4e11f5d42a4f40af88573320d00a
Victor Gardrinier <v.gardrinier@gmail.com>
2019-06-24 17:47:59 +0200

    removed client json file from gitignore and removed user details from app.rb

--- app.rb
+++ app.rb
@@ -17,11 +17,10 @@ post "/" do
     worksheet.insert_rows(worksheet.num_rows + 1, [new_row])
     worksheet.save
     erb :thanks
-  rescue StandardError => error
-    puts error.inspect
+  rescue
     erb :index, locals: {
             error_message: "Your details could not be saved, please try again.",
-            user_details: user_details
+
           }
   end
 end

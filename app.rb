a34aa2e5cd29c88505b039e14fd665eced89b689
Victor Gardrinier <v.gardrinier@gmail.com>
2019-06-30 16:06:29 +0200

    uodateed form

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ def worksheet
 end
 
 post "/" do
-  new_row = [params["name"], params["email"], params["phone_number"], params["address"]]
+  new_row = [params["first_name"], params["last_name"], params["email"], params["number"]]
   begin
     worksheet.insert_rows(worksheet.num_rows + 1, [new_row])
     worksheet.save
